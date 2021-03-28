85fb2dfe1739878fef60a05445a1fe7b59a9b0a0
mdumorfarukjahangir <ufjahangir96@gmail.com>
2021-03-28 23:45:13 +0600

    Fixed Some Error with Random post

--- index.php
+++ index.php
@@ -15,6 +15,9 @@ get_header();
 						$args = array(  
 							'post_type' => 'imagegallery',
 							'post_status' => 'publish',
+							'orderby'   => 'rand',
+							'posts_per_page' => 5,
+							
 						 
 							 
 							
